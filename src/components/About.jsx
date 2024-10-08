c6c2450fd528b70a8126ff27e4e4bcc202b9ca82
Adonay <samueldesta323@gmail.com>
2024-10-08 11:56:18 +0300

    fixed responsiveness of about page

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,7 +4,7 @@ const About = () => {
     return (
         <div className="w-[100vw] h-screen bg-[#075985] overflow-clip font-roboto">
             <div className="w-[100%] h-[50%] translate-y-[-11%]  rounded-bl-[50vw] bg-[#d9d9d9] flex flex-col justify-between">
-                <div className="w-[100%] h-[45%]  flex items-center justify-end pr-32 pt-24">
+                <div className="w-[100%] h-[45%]  flex items-center justify-end lg:pr-32 pr-10 pt-24">
                     <Link to='/' className="p-3 bg-[#d9d9d9] rounded-md border-2 border-black">
                         <button>Back To Home</button>
                     </Link>
@@ -16,22 +16,22 @@ const About = () => {
                 </div>
 
             </div>
-            <div className="w-[100%] h-[50%] flex flex-col justify-between *:*:text-center">
+            <div className="w-[100%] min-h-[50%] flex flex-col justify-between *:*:text-center">
                 <div className="w-[100%] h-[45%]  flex justify-center">
                     <p className="w-[70%] text-center text-[22px] text-white">We specialize in providing unforgettable experiences in Ethiopia, from breathtaking landscapes to rich cultural heritage.</p>
                 </div>
-                <div className="w-[100%] h-[45%] flex items-center justify-evenly *:gap-3">
+                <div className="w-[100%] h-[45%] flex items-start justify-evenly *:gap-3">
 
-                    <div className="w-[10%] *:w-[100%] flex flex-col items-center justify-between ">
+                    <div className="lg:w-[10%] w-[20%] *:w-[100%] flex flex-col items-center justify-between ">
                         <p className="pt-3 pb-3  bg-white text-[#075985] rounded-lg text-center text-[20px] font-bold">100+</p>
                         <p className="text-white text-[18px] font-bold">Tour Travels</p>
                     </div>
-                    <div className="w-[10%] *:w-[100%]  flex flex-col items-center justify-between">
+                    <div className="lg:w-[10%] w-[20%] *:w-[100%]  flex flex-col items-center justify-between">
                         <p className="pt-3 pb-3  bg-white text-[#075985] rounded-lg text-center text-[20px] font-bold">500+</p>
                         <p className="text-white text-[18px] font-bold">Tour Sites</p>
                         
                     </div>
-                    <div className="w-[10%] *:w-[100%]  flex flex-col items-center justify-between">
+                    <div className="lg:w-[10%] w-[20%] *:w-[100%]  flex flex-col items-center justify-between">
                         <p className="pt-3 pb-3  bg-white text-[#075985] rounded-lg text-center text-[20px] font-bold">3500+</p>
                         <p className="text-white text-[18px] font-bold">Total Clients</p>
                         
